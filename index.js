626ba0f7df9e4b0f91aac8757dfcbea1d6632d9f
Brian Branch <reisuj@gmail.com>
2019-08-16 15:39:27 -0400

    Initial Commit Diving into DOM

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-alert("hello");
+// alert("hello");
+var listItem3 = document.firstElementChild.lastElementChild.forthElementChild.lastElementChild;
+listItem3.innerHTML = "I Changed This with Javascript";
